a370f790187568d206e6ac2a6f4f123d70a9302c
ostaoleho <oleh.ostashevskyi@otakoyi.com.ua>
2021-09-23 10:25:33 +0300

    add font-size

--- src/frontend/src/app/App.components/HeaderAuth/HeaderAuth.view.tsx
+++ src/frontend/src/app/App.components/HeaderAuth/HeaderAuth.view.tsx
@@ -3,41 +3,47 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import { PublicUser } from 'shared/user/PublicUser'
 
-import { Button } from "../Button/Button.controller";
+import { Button } from '../Button/Button.controller'
 import { HamburgerLeft } from '../Hamburger/Hamburger.controller'
 // prettier-ignore
 import { HeaderLoggedIn, HeaderLoggedOut, HeaderLogo, HeaderMenuItem, HeaderStyled } from "./HeaderAuth.style";
-
+import Arrow from '../../../assets/Vector.png'
+import Logo from '../../../assets/logo.png'
 
 type HeaderViewProps = {
-    user?: PublicUser
-    removeAuthUserCallback: () => void
+  user?: PublicUser
+  removeAuthUserCallback: () => void
 }
 
 // Overall Navbar
-export const HeaderView = ({user, removeAuthUserCallback}: HeaderViewProps) => {
-    return (
-        // <HeaderStyled> 
-        //         <HamburgerLeft/>
-        //     <Link to="/">
-        //         <HeaderLogo alt="logo" width="100px" src="/logo.svg"/>
-        //     </Link>
-        //     {user ? loggedInHeader({user, removeAuthUserCallback}) : loggedOutHeader()}
-        // </HeaderStyled>
-        <div className = 'header-auth'>
-            <div className="header-auth-button">
-                <button>&#x2190;</button>
-            </div>
-            <div className="header-auth-logo"></div>
-            <div className="header-auth-"></div>
-        </div>
-    )
+export const HeaderView = ({ user, removeAuthUserCallback }: HeaderViewProps) => {
+  return (
+    // <HeaderStyled>
+    //         <HamburgerLeft/>
+    //     <Link to="/">
+    //         <HeaderLogo alt="logo" width="100px" src="/logo.svg"/>
+    //     </Link>
+    //     {user ? loggedInHeader({user, removeAuthUserCallback}) : loggedOutHeader()}
+    // </HeaderStyled>
+    <div className="header-auth">
+      <div className="header-auth-button">
+        <button>
+          <img src={Arrow} alt="arrow" width="12" height="12" />
+          <span>Back</span>
+        </button>
+      </div>
+      <div className="header-auth-logo">
+        <img src={Logo} alt="" />
+      </div>
+      <div className="header-auth-sign">some text</div>
+    </div>
+  )
 }
 
 function loggedOutHeader() {
   return (
     <HeaderLoggedOut>
-        {/* <Link className={'get-started'} to="/chainlinkIntroduction/chapter-1">
+      {/* <Link className={'get-started'} to="/chainlinkIntroduction/chapter-1">
             <Button text="GET STARTED" color="secondary"/>
         </Link> */}
       {/*<Link to="/terms">*/}
@@ -53,30 +59,30 @@ function loggedOutHeader() {
   )
 }
 
-function loggedInHeader({user, removeAuthUserCallback}: HeaderViewProps) {
-    return (
-        <HeaderLoggedIn>
-            {/*<Link to="/terms">*/}
-            {/*    <HeaderMenuItem>TERMS</HeaderMenuItem>*/}
-            {/*</Link>*/}
-            <Link to={`/user/${user?.username}`}>
-                <HeaderMenuItem>{user?.username}</HeaderMenuItem>
-            </Link>
-            <Link
-                to="/"
-                onClick={() => {
-                    removeAuthUserCallback()
-                }}
-            >
-                <HeaderMenuItem>LOGOUT</HeaderMenuItem>
-            </Link>
-        </HeaderLoggedIn>
-    )
+function loggedInHeader({ user, removeAuthUserCallback }: HeaderViewProps) {
+  return (
+    <HeaderLoggedIn>
+      {/*<Link to="/terms">*/}
+      {/*    <HeaderMenuItem>TERMS</HeaderMenuItem>*/}
+      {/*</Link>*/}
+      <Link to={`/user/${user?.username}`}>
+        <HeaderMenuItem>{user?.username}</HeaderMenuItem>
+      </Link>
+      <Link
+        to="/"
+        onClick={() => {
+          removeAuthUserCallback()
+        }}
+      >
+        <HeaderMenuItem>LOGOUT</HeaderMenuItem>
+      </Link>
+    </HeaderLoggedIn>
+  )
 }
 
 HeaderView.propTypes = {
-    user: PropTypes.object,
-    removeAuthUserCallback: PropTypes.func.isRequired,
+  user: PropTypes.object,
+  removeAuthUserCallback: PropTypes.func.isRequired,
 }
 
 HeaderView.defaultProps = {}
