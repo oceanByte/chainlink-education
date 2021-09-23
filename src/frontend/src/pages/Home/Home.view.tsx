3e2d99befd79999842bf78b178034500bafaeccb
Stan <stanislav.martynov@otakoyi.com.ua>
2021-09-23 17:48:11 +0300

    Made NavBar (Header) Responsive. Continued creating HomePage, made its first part responsive.

--- src/frontend/src/pages/Home/Home.view.tsx
+++ src/frontend/src/pages/Home/Home.view.tsx
@@ -4,18 +4,39 @@ import * as React from 'react'
 export const HomeView = () => {
   return (
     <div className='home'>
-      <div className='home-intro mt-96'>
-        <div className='home-intro-content'>
-          <div className='home-intro-content__header'>Learn to program with Chainlink Academy</div>
-          <div className='home-intro-content__paragraph mt-30'>
-            Chainlink's decentralized oracle network provides reliable, tamper-proof
-            inputs and outputs for complex smart contracts on any blockchain
+      <div className='home-wrapper-intro'>
+        <div className='home-intro'>
+          <div className='home-intro-content'>
+            <div className='home-intro-content__header'>Learn to program with Chainlink Academy</div>
+            <div className='home-intro-content__paragraph mt-30'>
+              Chainlink's decentralized oracle network provides reliable, tamper&#8209;proof
+              inputs and outputs for complex smart contracts on any blockchain
+            </div>
+            <button className='home-intro-content__button btn mt-40'>
+              <span className='home-intro-content__button__text'> Get started for FREE </span><span className='arrow-upright' />
+            </button>
           </div>
-          <button className='home-intro-content__button btn mt-40'>Get started for FREE</button>
+          <div className='home-intro__image' />
         </div>
-        <div className='home-intro__image' />
       </div>
       <div className='home-ellipse' />
+      <div className='home-nft'>
+        <div className='home-nft__image' />
+        <div className='home-nft-content'>
+          <div className='home-nft-content__header'>
+            <div className='home-nft-content__header__number'>01</div>
+            <div className='home-nft-content__header__line' />
+          </div>
+          <div className='home-nft-content__text'>
+            <div className='home-nft-content__text__header'>
+              NFT Certificate
+            </div>
+            <div className='home-nft-content__text__paragraph'>
+              Complete the course and receive your certificate as a Non-Fungible Token
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
