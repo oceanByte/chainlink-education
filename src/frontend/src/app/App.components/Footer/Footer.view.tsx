eb16d2f19b79eb70d09a35d4aab31c08f8f66722
Albert Peci <albert.peci@posteo.de>
2022-01-24 22:18:15 +0100

    fix: add rel="noopener noreferrer"

--- src/frontend/src/app/App.components/Footer/Footer.view.tsx
+++ src/frontend/src/app/App.components/Footer/Footer.view.tsx
@@ -5,29 +5,29 @@ export const FooterView: any = () => (
   <div className="footer">
     <div className="footer-menu footer-content">
       <div className="footer-menu-list p-font">
-        <a href="https://chainlink-education-app.herokuapp.com/" target="_blank" className="footer-menu-list__item">
+        <a href="https://chainlink-education-app.herokuapp.com/" target="_blank" rel="noopener noreferrer" className="footer-menu-list__item">
           Academy
         </a>
-        <a href="https://chain.link/" target="_blank" className="footer-menu-list__item">
+        <a href="https://chain.link/" target="_blank" rel="noopener noreferrer" className="footer-menu-list__item">
           Ecosystem
         </a>
-        <a href="mailto:chainlink@academy.io" target="_blank" className="footer-menu-list__item">
+        <a href="mailto:chainlink@academy.io" target="_blank" rel="noopener noreferrer" className="footer-menu-list__item">
           Contact
         </a>
-        <a href="https://github.com/chainlink-education/academy" target="_blank" className="footer-menu-list__item">
+        <a href="https://github.com/chainlink-education/academy" target="_blank" rel="noopener noreferrer" className="footer-menu-list__item">
           GitHub
         </a>
-        <a href="https://docs.chain.link/" target="_blank" className="footer-menu-list__item">
+        <a href="https://docs.chain.link/" target="_blank" rel="noopener noreferrer" className="footer-menu-list__item">
           Documentation
         </a>
-        <a href="https://chain.link/community/events" target="_blank" className="footer-menu-list__item mr-0">
+        <a href="https://chain.link/community/events" target="_blank" rel="noopener noreferrer" className="footer-menu-list__item mr-0">
           Upcoming events
         </a>
       </div>
       <div className="footer-menu-social-media">
-        <a href="https://discord.com/invite/aSK4zew" target="_blank" className="footer-menu-social-media__item item-discord" />
-        <a href="https://t.me/chainlinkofficial" target="_blank" className="footer-menu-social-media__item item-telegram" />
-        <a href="https://twitter.com/chainlink" target="_blank" className="footer-menu-social-media__item item-twitter mr-0" />
+        <a href="https://discord.com/invite/aSK4zew" target="_blank" rel="noopener noreferrer" className="footer-menu-social-media__item item-discord" />
+        <a href="https://t.me/chainlinkofficial" target="_blank" rel="noopener noreferrer" className="footer-menu-social-media__item item-telegram" />
+        <a href="https://twitter.com/chainlink" target="_blank" rel="noopener noreferrer" className="footer-menu-social-media__item item-twitter mr-0" />
       </div>
     </div>
     <div className="footer-separator">
@@ -54,9 +54,9 @@ export const FooterView: any = () => (
         </Link>
       </div>
       <div className="footer-credentials-social-media">
-        <a href="https://discord.com/invite/aSK4zew" className="footer-credentials-social-media__item item-discord" />
-        <a href="https://t.me/chainlinkofficial" className="footer-credentials-social-media__item item-telegram" />
-        <a href="https://twitter.com/chainlink" className="footer-credentials-social-media__item item-twitter mr-0" />
+        <a href="https://discord.com/invite/aSK4zew" rel="noopener noreferrer" className="footer-credentials-social-media__item item-discord" />
+        <a href="https://t.me/chainlinkofficial" rel="noopener noreferrer" className="footer-credentials-social-media__item item-telegram" />
+        <a href="https://twitter.com/chainlink" rel="noopener noreferrer" className="footer-credentials-social-media__item item-twitter mr-0" />
       </div>
     </div>
   </div>
