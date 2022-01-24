1aaf93d11e977d7f5f948d4d613dad4974e436ce
Albert Peci <albert.peci@posteo.de>
2022-01-24 20:46:45 +0100

    refactor: remove additional tracking

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -3,10 +3,8 @@ import ReactGA from 'react-ga'
 import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3'
 // @ts-ignore
 import TagManager from 'react-gtm-module'
-import { hotjar } from 'react-hotjar'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import TwitterConvTrkr from 'react-twitter-conversion-tracker'
 
 import { App } from './app/App.controller'
 import { configureStore } from './app/App.store'
@@ -20,15 +18,11 @@ import './styles/main.scss'
 export const store = configureStore({})
 
 ReactGA.initialize('UA-XXXXXXXX-1')
-hotjar.initialize(111111, 1)
 
 const tagManagerArgs = {
   gtmId: process.env.REACT_APP_GTM_ID,
 }
 TagManager.initialize(tagManagerArgs)
-
-TwitterConvTrkr.init('xxxxx')
-TwitterConvTrkr.pageView()
 
 export const Root = () => {
   return (
