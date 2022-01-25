2b38ad45b699aba5b8518fe036d2d9cfce818a84
Albert Peci <albert.peci@posteo.de>
2022-01-25 12:41:18 +0100

    update certificate check

--- src/api/src/resolvers/user/isCertified/isCertified.ts
+++ src/api/src/resolvers/user/isCertified/isCertified.ts
@@ -14,7 +14,7 @@ export const isCertified = async (ctx: Context, next: Next): Promise<void> => {
   const user = await UserModel.findOne({ name: { $regex: new RegExp("^" + name.toLowerCase(), "i") } }).lean()
 
   if (user) userFound = true
-  if (user && user.progress && user.progress.length && user.progress.length >= 20) isCertified = true
+  if (user && user.progress && user.progress.length && user.progress.length >= 7) isCertified = true
 
   const response: IsCertifiedOutputs = { userFound, isCertified }
 
